d4d4d1dcbb1d440f297a150ea3ecf4c35f063f66
Aleksandr Markov <bast.ne@mail.ru>
2023-02-25 22:04:49 +0700

    Task 3 Numbers

---
+++ Lesson_1/1_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введиде произвольное число: ");
+int num = int.Parse(Console.ReadLine()!);
+int reverse = -num;
+
+while (reverse <= num)
+{
+    Console.Write(reverse + " ");
+    reverse++;
+}
