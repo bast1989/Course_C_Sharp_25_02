3ba4e0f076a5c346206186034866a351fc35d433
Aleksandr Markov <bast.ne@mail.ru>
2023-02-25 21:28:14 +0700

    Task 2 Week

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число соответствующее дню недели: ");
+int day = int.Parse(Console.ReadLine()!);
+
+if (day == 3) Console.WriteLine("Среда");
+else if (day == 5) Console.WriteLine("Пятница");
+else Console.WriteLine("Я незнаю такого дня недели.");
