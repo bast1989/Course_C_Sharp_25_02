1b971e44b95d5de45bc339d671e827eea5dbe425
Aleksandr Markov <bast.ne@mail.ru>
2023-02-27 01:06:08 +0700

    Task 2_3

---
+++ Lesson_2/HW_2_3/Program.cs
@@ -0,0 +1,20 @@
+﻿void func(int num)
+{
+    if(num < 100)
+    {
+        Console.WriteLine("Тоетьей цифры нет");
+    }
+    else
+    {
+        while(num > 999)
+        {
+            num /= 10;
+        }
+        num %= 10;
+        Console.WriteLine(num);
+    }
+}
+
+func(645);
+func(78);
+func(32679);
