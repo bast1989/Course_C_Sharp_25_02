b13db0decbf56b991ea05782e9eb9d65f352c3f4
Aleksandr Markov <bast.ne@mail.ru>
2023-02-27 00:18:21 +0700

    HW Task 2_1

---
+++ Lesson_2/HW_2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int func(int num)
+{
+    if(num < 0) num = -num;
+    while(num > 100)
+    {
+        num /= 10;
+    }
+    int res = num % 10;
+    return res;
+
+}
+
+Console.WriteLine(func(456));
+Console.WriteLine(func(782));
+Console.WriteLine(func(918));
