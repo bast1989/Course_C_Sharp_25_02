03bd1e4548cedcdab28207fedb421001db72531e
Aleksandr Markov <bast.ne@mail.ru>
2023-03-05 20:09:17 +0700

    Task 3_3_add_new_version

--- Lesson_3/3_3/Program.cs
+++ Lesson_3/3_3/Program.cs
@@ -10,3 +10,110 @@
     return res;
 }
 Console.WriteLine(SqrStr(5));
+
+
+
+
+
+
+
+
+
+
+
+
+void Squares(int num)
+{
+    int i = 1;
+    Console.Write($"{num} -> ");
+
+    while (num >= i)
+    {
+        Console.Write($"{Math.Pow(i, 2)}, ");
+        i++;
+    }
+    Console.WriteLine();
+}
+
+int num = int.Parse(Console.ReadLine()!);
+Squares(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2 вариант
+
+void Squares2(int n)
+{
+    double[] nums = new double[n];
+    int index = 0;
+
+    Console.Write($"{n} -> ");
+
+    while (index < n)
+    {
+        nums[index] = Math.Pow((index + 1), 2);
+        Console.Write($"{nums[index]}, ");
+        index += 1;
+    }    
+    Console.WriteLine();
+}
+
+int num1 = int.Parse(Console.ReadLine()!);
+Squares2(num);
+
+
+
+
+
+
+
+
+
+
+
+
+// 3 вариант
+
+double[] Squares3(int n)
+{
+    double[] nums = new double[n];
+    int index = 0;
+
+    while (index < n)
+    {
+        nums[index] = Math.Pow((index + 1), 2);
+        index += 1;
+    }
+
+    return nums;
+}
+
+void PrintList(int num)
+{
+    double[] arr = Squares3(num); 
+    int index = 0;
+    
+    Console.Write($"{num} -> ");
+
+    while (index < arr.Length)
+    {
+        Console.Write($"{arr[index]}, ");
+        index += 1;
+    }
+}
+
+int num2 = int.Parse(Console.ReadLine()!);
+PrintList(num);
