e9a7b7d9615579019e62e74ab6a4f341fd6c2055
Aleksandr Markov <bast.ne@mail.ru>
2023-03-05 16:18:45 +0700

    Task 3_3

---
+++ Lesson_3/3_3/Program.cs
@@ -0,0 +1,12 @@
+﻿string SqrStr(int num)
+{
+    string res = "";
+    for(int i = 1; i <= num; i++)
+    {
+        double sq = Math.Pow(i, 2);
+        res += sq.ToString() + " ";
+    }
+
+    return res;
+}
+Console.WriteLine(SqrStr(5));
