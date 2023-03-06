fd4e8b2d6be1fb37558c4104c04bb61c64a3b0c7
Aleksandr Markov <bast.ne@mail.ru>
2023-03-06 15:08:43 +0700

    HW Task 4_1

---
+++ Lesson_4/4_1/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 1: Напишите программу, которая принимает 
+//на вход число и выдаёт количество цифр в числе.
+
+
+//Своё решение
+
+int Number(int num)
+{
+    int i = 0;
+    for(i = 0; num > 0; i++)
+    {
+        num /= 10;
+    }
+    return i;
+}
+
+int a = Number(456);
+Console.WriteLine(a);
+a = Number(78);
+Console.WriteLine(a);
+a = Number(89126);
+Console.WriteLine(a);
+a = Number(123456);
+Console.WriteLine(a);
+
+
+
+int SumNum(int num)
+{
+    int kol = 0;
+    for(int i = 1; num > 0; i++)
+    {
+        num = num / 10;
+        kol = i;
+    }
+    return kol;
+}
+
+Console.WriteLine(SumNum(456));
