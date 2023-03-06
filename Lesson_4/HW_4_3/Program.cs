93dc183ff9353f2d585c9ce6f52dd46d5baf55d4
Aleksandr Markov <bast.ne@mail.ru>
2023-03-06 21:36:56 +0700

    HW Task 4_3

---
+++ Lesson_4/HW_4_3/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] RandArr(int num)
+{
+    int[] numArr = new int[num];
+    for(int i = 0; i < numArr.Length; i++)
+    {
+        numArr[i] = new Random().Next(1, 35);
+        //Console.WriteLine(numArr[i]);
+    }
+    return numArr;
+}
+
+void DoublePrintArr(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+
+    Console.Write("-> ")
+    ;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+DoublePrintArr(RandArr(8));
