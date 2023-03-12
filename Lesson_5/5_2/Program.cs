eb966169fc647c097d39724c86790b71634c773e
Aleksandr Markov <bast.ne@mail.ru>
2023-03-12 17:33:36 +0700

    Task 5_2_v2

--- Lesson_5/5_2/Program.cs
+++ Lesson_5/5_2/Program.cs
@@ -1,4 +1,8 @@
-﻿int[] GenArr(int num, int min, int max)
+﻿// 2. Задайте массив. Напишите программу, которая определяет,
+//    присутствует ли заданное число в массиве.
+
+/*
+int[] GenArr(int num, int min, int max)
 {
     int[] intArr = new int[num];
 
@@ -35,3 +39,40 @@ PrintArr(myArr);
 Console.Write("-> ");
 string numTo = NumToArr(myArr, 7);
 Console.Write(numTo);
+
+*/
+
+int[] GenArr(int num, int min, int max)
+{
+    int[] intArr = new int[num];
+
+    for(int i = 0; i < intArr.Length; i++)
+    {
+        intArr[i] = new Random().Next(min, max + 1);        
+    }
+
+    return intArr;
+}
+
+void PrintArr(int[] intArr)
+{
+    for(int i = 0; i < intArr.Length; i++)
+    {
+        Console.Write(intArr[i] + " ");
+    }
+}
+
+string NumToArr(int[] intArr, int num)
+{    
+    for(int i = 0; i < intArr.Length; i++)
+    {
+        if(intArr[i] == num) return "да";
+    }
+    return "нет";
+}
+
+int[] newArr = GenArr(8, -8, 8);
+PrintArr(newArr);
+Console.Write("-> ");
+string numTo = NumToArr(newArr, 7);
+Console.WriteLine(numTo);
