cde2f993fb1ff64e92042be91e5eedad02a2fc15
Aleksandr Markov <bast.ne@mail.ru>
2023-03-12 17:09:15 +0700

    Task 5_2

---
+++ Lesson_5/5_2/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] GenArr(int num, int min, int max)
+{
+    int[] intArr = new int[num];
+
+    for(int i = 0; i < intArr.Length; i++)
+    {
+        intArr[i] = new Random().Next(min, max + 1);
+        //Console.WriteLine(intArr[i]);
+    }
+
+    return intArr;
+}
+
+string NumToArr(int[] intArr, int num)
+{
+    int count = 0;
+    for(int i = 0; i < intArr.Length; i++)
+    {
+        if(intArr[i] == num) count++;
+    }
+    if(count > 0) return "да";
+    else return "нет";
+}
+
+void PrintArr(int[] intArr)
+{
+    for(int i = 0; i < intArr.Length; i++)
+    {
+        Console.Write(intArr[i] + " ");
+    }
+}
+
+int[] myArr = GenArr(8, -8, 8);
+PrintArr(myArr);
+Console.Write("-> ");
+string numTo = NumToArr(myArr, 7);
+Console.Write(numTo);
