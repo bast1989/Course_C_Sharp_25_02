7abe89528ec69023f4af8cbd9af3032b98ad5c1c
Aleksandr Markov <bast.ne@mail.ru>
2023-03-12 19:57:14 +0700

    HW Task 5_1

---
+++ Lesson_5/HW_5_1/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] GenArr(int num, int min, int max)
+{
+    int[] intArr = new int[num];
+
+    for(int i = 0; i < intArr.Length; i++)
+    {
+        intArr[i] = new Random().Next(min, max + 1);        
+    }
+
+    return intArr;
+}
+
+void PrintArr(int[] intArr)
+{
+    for(int i = 0; i < intArr.Length; i++)
+    {
+        Console.Write(intArr[i] + " ");
+    }
+}
+
+int EvenNumArr(int[] intArr)
+{
+    int count = 0;
+    for (int i = 0; i < intArr.Length; i++)
+    {
+        if(intArr[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] myArr = GenArr(6, 100, 999);
+PrintArr(myArr);
+Console.Write("-> ");
+int even = EvenNumArr(myArr);
+Console.Write(even);
