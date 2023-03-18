a98e9f880377b0a00f2685d9ae8c52751ae6e89c
Aleksandr Markov <bast.ne@mail.ru>
2023-03-18 22:27:41 +0700

    Task 7_4

---
+++ Lesson_7/7_4/Program.cs
@@ -0,0 +1,54 @@
+﻿int[,] GenDoubArr(int rang_0, int rang_1, int min, int max)
+{
+    int[,] arr = new int[rang_0, rang_1];
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintDoubArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+string ContainDoubArr(int[,] arr, int num)
+{
+    string text = "";    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if(arr[i, j] == num)
+            {
+                text = $"Введенный элемент {num}, результат: [{i}, {j}]";
+                goto Found;
+            }
+            else
+            {
+                text = $"Введенный элемент {num}, результат: такого элемента нет";
+            }
+        }
+    }
+    Found: return text;
+    
+}
+
+int[,] myArr = GenDoubArr(3, 3, 1, 9);
+PrintDoubArr(myArr);
+string containNum = ContainDoubArr(myArr, 6);
+Console.WriteLine(containNum);
+
+
+
