a5fc16f2be93c0fd1191cbccde33219db6039601
Aleksandr Markov <bast.ne@mail.ru>
2023-03-18 21:44:34 +0700

    Task 7_0

---
+++ Lesson_7/7_0/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] GenDoubArr(int rang_0, int rang_1, int min, int max)
+{
+    int[,] arr = new int[rang_0, rang_1];
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintDoubArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+int[,] myArr = GenDoubArr(3, 4, -8, 64);
+PrintDoubArr(myArr);
+
