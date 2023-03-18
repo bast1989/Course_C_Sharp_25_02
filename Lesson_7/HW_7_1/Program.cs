23a63f667f02c76ebccc6d3f34b0c6bbf16b076d
Aleksandr Markov <bast.ne@mail.ru>
2023-03-18 23:22:17 +0700

    HW Task 7_1

---
+++ Lesson_7/HW_7_1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+double[,] GenDoubArr(int rang_0, int rang_1, double min, double max)
+{
+    double[,] arr = new double[rang_0, rang_1];
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Math.Round(min + (new Random().NextDouble() * (max - min)), 1);
+        }
+    }
+    return arr;
+}
+
+void PrintDoubArr(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+double[,] myArr = GenDoubArr(3, 4, -10, 10);
+PrintDoubArr(myArr);
+
