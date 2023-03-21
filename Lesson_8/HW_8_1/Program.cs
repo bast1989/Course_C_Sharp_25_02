8962f200c0aca6e5340151d755856246340198fa
Aleksandr Markov <bast.ne@mail.ru>
2023-03-21 19:24:18 +0700

    HW Task 8_1

---
+++ Lesson_8/HW_8_1/Program.cs
@@ -0,0 +1,50 @@
+﻿int[,] GenDoubArr(int rang_0, int rang_1, int maiRand, int maxRand)
+{
+    int[,] arr = new int[rang_0, rang_1];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(maiRand, maxRand + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintDoubArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void OrderedArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1) - 1; k++)
+            {
+                if (arr[i, k] < arr[i, k + 1])
+                {
+                    int temp = arr[i, k + 1];
+                    arr[i, k + 1] = arr[i, k];
+                    arr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+int[,] myArr = GenDoubArr(5, 5, 0, 9);
+PrintDoubArr(myArr);
+Console.WriteLine("\n");
+OrderedArr(myArr);
+PrintDoubArr(myArr);
