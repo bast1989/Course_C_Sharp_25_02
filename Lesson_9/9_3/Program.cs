79951eda033e5dd123e4883ef588652dcba0bc36
Aleksandr Markov <bast.ne@mail.ru>
2023-03-26 00:25:04 +0700

    Task 9_3

---
+++ Lesson_9/9_3/Program.cs
@@ -0,0 +1,11 @@
+﻿int DegNum(int A, int B)
+{
+    if(B == 0) return 1;
+    return DegNum(A, B - 1) * A;
+}
+
+
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+int deg = DegNum(num_1, num_2);
+Console.WriteLine(deg);
