bcf47e895c105abf384243891d931d51919e24a1
Aleksandr Markov <bast.ne@mail.ru>
2023-03-26 00:10:01 +0700

    Task 9_2

---
+++ Lesson_9/9_2/Program.cs
@@ -0,0 +1,10 @@
+﻿int SumNum(int N)
+{
+    if(N == 0) return 0;
+    return SumNum(N / 10) + N % 10;
+}
+
+
+int num = int.Parse(Console.ReadLine()!);
+int sum = SumNum(num);
+Console.WriteLine(sum);
