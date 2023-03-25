f24fa467f1b338ed7368c04b21b2da1773cd4000
Aleksandr Markov <bast.ne@mail.ru>
2023-03-26 00:38:22 +0700

    HW Task 9_1

---
+++ Lesson_9/HW_9_1/Program.cs
@@ -0,0 +1,12 @@
+﻿void ValueSet(int start, int finish)
+{
+    if(finish == start - 1) return;
+    ValueSet(start, finish - 1);
+    if(finish % 2 == 0) Console.Write($"{finish} ");
+}
+
+
+
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+ValueSet(num_1, num_2);
